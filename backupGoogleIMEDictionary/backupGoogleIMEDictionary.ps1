04215fd089448b1b2f327a1f6cfee279c058ff9b
M424 <mys40651147@gmail.com>
2023-11-22 17:12:51 +0900

    GoogleIME辞書ファイルをGoogleDrive上にバックアップ/リストアするスクリプト

---
+++ backupGoogleIMEDictionary/backupGoogleIMEDictionary.ps1
@@ -0,0 +1,10 @@
+﻿# load トースト通知
+. "$PSScriptRoot\..\common\Toast.ps1"
+
+# Google IME 辞書ファイル
+$from = "${env:USERPROFILE}\AppData\LocalLow\Google\Google Japanese Input\user_dictionary.db"
+# バックアップ先
+$to = "G:\マイドライブ\ユーザー辞書Backup\"
+
+Copy-Item $from -Destination $to
+Toast -title "Google IME" -msg "辞書ファイルをGoogleDriveにバックアップしました"
---
+++ backupGoogleIMEDictionary/restoreGoogleIMEDictionary.ps1
@@ -0,0 +1,16 @@
+﻿# load トースト通知
+. "$PSScriptRoot\..\common\Toast.ps1"
+
+# バックアップ先
+$from = "G:\マイドライブ\ユーザー辞書Backup\user_dictionary.db"
+# Google IME 辞書ファイル
+$to = "${env:USERPROFILE}\AppData\LocalLow\Google\Google Japanese Input\"
+
+# リストア
+Copy-Item $from -Destination $to
+
+# Google IME 辞書を開き、更新する
+$googleime = "${env:ProgramFiles(x86)}\Google\Google Japanese Input\GoogleIMEJaTool.exe"
+start -FilePath $googleime -ArgumentList "--mode=dictionary_tool"
+
+Toast -title "Google IME" -msg "辞書ファイルをGoogleDriveからリストアしました"
